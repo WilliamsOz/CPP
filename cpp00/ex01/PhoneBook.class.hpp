11dd3be0f895562327868745f9d04a2fa7cc6606
auto school <autoschool@iMac-de-auto.local>
2022-05-16 17:01:08 +0200

    Creating arbo

---
+++ cpp00/ex01/PhoneBook.class.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.class.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/16 16:53:16 by wiozsert          #+#    #+#             */
+/*   Updated: 2022/05/16 17:00:30 by wiozsert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_CLASS_HPP
+# define PHONEBOOK_CLASS_HPP
+
+class Phonebook
+{
+	public :
+	int lol;
+
+	private :
+
+};
+
+#endif
---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/16 14:45:37 by wiozsert          #+#    #+#             */
+/*   Updated: 2022/05/16 17:00:47 by wiozsert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Contact.class.hpp"
+#include "PhoneBook.class.hpp"
+
+int main(void)
+{
+	Phonebook xd;
+
+	xd.lol = 42;
+	std::cout << xd.lol << std::endl;
+
+	return (0);
+}
