2f59d2e36569078e2a5cfa147b082cb4d1709964
Williams Ozserttas <willyiony@zero.home>
2022-09-14 10:13:00 +0200

    Add some color

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
+/*   By: willyiony <willyiony@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 16:00:58 by wiozsert          #+#    #+#             */
-/*   Updated: 2022/05/24 16:08:58 by wiozsert         ###   ########.fr       */
+/*   Updated: 2022/09/14 10:12:38 by willyiony        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,16 @@
 int	main(void)
 {
 	std::string string = "HI THIS IS BRAIN";
-	std::string *stringPTR = &string;
-	std::string &stringREF = string;
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
 
-	std::cout << "The memory address of the string variable is : " << "|" << &string << "|" << std::endl;
-	std::cout << "The memory address held by stringPTR is : " << "|" << stringPTR << "|" << std::endl;
-	std::cout << "The memory address held by stringREF is : " << "|" << &stringREF << "|" << std::endl;
-	std::cout << std::endl;
+	std::cout << "\033[1;35mThe memory address of attribut string is : " << "|" << &string << "|" << std::endl;
+	std::cout << "The memory address of attribut stringPTR is : " << "|" << stringPTR << "|" << std::endl;
+	std::cout << "The memory address of attribut stringREF is : " << "|" << &stringREF << "|" "\033[1;32m" << std::endl << std::endl;
 
-	std::cout << "The value of the string variable is : " << "|" << string << "|" << std::endl;
-	std::cout << "The value pointed to by stringPTR is : " << "|" << *stringPTR << "|" << std::endl;
-	std::cout << "The value pointed to by stringREF is : " << "|" << stringREF << "|" << std::endl;
-	
+	std::cout << "\033[1;32mThe value of the attribut string is : " << "|" << string << "|" << std::endl;
+	std::cout << "The value pointed by stringPTR is : " << "|" << *stringPTR << "|" << std::endl;
+	std::cout << "The value pointed by stringREF is : " << "|" << stringREF << "|" << "\033[1;32m" << std::endl;
 
 	return (0);
 }
