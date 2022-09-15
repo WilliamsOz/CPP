8729f62f897483536a0d8421604ad36374a5b913
Williams Ozserttas <willyiony@zero.home>
2022-09-15 10:49:13 +0200

    remove comment to help start in main

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,19 +6,12 @@
 /*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 14:42:35 by wiozsert          #+#    #+#             */
-/*   Updated: 2022/09/14 18:33:05 by wiozsert         ###   ########.fr       */
+/*   Updated: 2022/09/15 10:49:00 by wiozsert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Sed.hpp"
 
-//av[1] = a filename and
-//av[2] && av[3] = two strings, s1 and s2
-//on ouvre filename, puis :
-// on copie son contenu dans un nouveau fichier : "filename.replace" en remplacer s1 par s2
-//std::string.replace est interdit, les fonction de manipulation en c aussi
-//gerer les erreurs des flux (fichier sans permssions, inexistant ...)
-//faire son propre main de test pour verifier
 int main(int ac, char **av)
 {
 	Sed	sed;
