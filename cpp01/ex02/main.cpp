dd7f452768247138788725c87d06f8ff2bf49a17
Williams OZSERTTAS <wiozsert@e1r12p2.clusters.42paris.fr>
2022-10-10 13:35:25 +0200

    End of ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 16:00:58 by wiozsert          #+#    #+#             */
-/*   Updated: 2022/09/14 12:34:25 by wiozsert         ###   ########.fr       */
+/*   Updated: 2022/10/10 13:34:58 by wiozsert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ int	main(void)
 
 	std::cout << "\033[1;35mThe memory address of attribut string is : " << "|" << &string << "|" << std::endl;
 	std::cout << "The memory address of attribut stringPTR is : " << "|" << stringPTR << "|" << std::endl;
-	std::cout << "The memory address of attribut stringREF is : " << "|" << &stringREF << "|" "\033[1;32m" << std::endl << std::endl;
+	std::cout << "The memory address of attribut stringREF is : " << "|" << &stringREF << "|" "\033[0m" << std::endl << std::endl;
 
 	std::cout << "\033[1;32mThe value of the attribut string is : " << "|" << string << "|" << std::endl;
 	std::cout << "The value pointed by stringPTR is : " << "|" << *stringPTR << "|" << std::endl;
-	std::cout << "The value pointed by stringREF is : " << "|" << stringREF << "|" << "\033[1;32m" << std::endl;
+	std::cout << "The value pointed by stringREF is : " << "|" << stringREF << "|" << "\033[0m" << std::endl;
 
 	return (0);
 }
