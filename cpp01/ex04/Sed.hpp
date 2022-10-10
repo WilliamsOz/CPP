457ae2eec640204cabde78975ce5c9aa5b67f898
Williams OZSERTTAS <wiozsert@e1r12p2.clusters.42paris.fr>
2022-10-10 13:29:03 +0200

    End of ex04

--- cpp01/ex04/Sed.hpp
+++ cpp01/ex04/Sed.hpp
@@ -6,7 +6,7 @@
 /*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 14:42:48 by wiozsert          #+#    #+#             */
-/*   Updated: 2022/09/23 12:04:02 by wiozsert         ###   ########.fr       */
+/*   Updated: 2022/10/10 13:28:15 by wiozsert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,17 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <sys/stat.h> // Get File Information
+#include <sys/stat.h>
 #include <ios>
 #include <fcntl.h>
 #include <unistd.h>
-#include "Color.hpp"
-#define PS(x) std::cout << x << std::endl;  //REMOVE
+#define REDCOLOR "\033[1;31m"
+#define GRNCOLOR "\033[1;32m"
+#define YELCOLOR "\033[1;33m"
+#define BLUECOLOR "\033[1;34m"
+#define MAGCOLOR "\033[1;35m"
+#define CYANCOLOR "\033[1;36m"
+#define ENDCOLOR "\033[0m"
 
 
 class	Sed
