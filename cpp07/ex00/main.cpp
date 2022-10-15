e5399b8e2c323da6ece342b30d2c001a90d8d558
Williams Ozserttas <willyiony@zero.local>
2022-10-15 11:50:11 +0200

    End of ex00

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,15 +6,158 @@
 /*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 11:09:59 by wiozsert          #+#    #+#             */
-/*   Updated: 2022/10/15 11:10:52 by wiozsert         ###   ########.fr       */
+/*   Updated: 2022/10/15 11:49:57 by wiozsert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
+void	intTest( void )
+{
+	int		x = 42;
+	int		y = 21;
+	int		res;
+
+	std::cout << GRNCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
+	std::cout << MAGCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
+
+	res = min(x, y);
+	std::cout << "Who is the min ? " << std::endl << MAGCOLOR << res << ENDCOLOR << std::endl;
+
+	res = max(x, y);
+	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
+
+	std::cout << "-------------------------------" << std::endl;
+	swap(x, y);
+	std::cout << MAGCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
+	std::cout << GRNCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
+
+	res = min(x, y);
+	std::cout << "Who is the min ? " << std::endl << MAGCOLOR << res << ENDCOLOR << std::endl;
+
+	res = max(x, y);
+	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
+	return ;
+}
+
+void	charTest( void )
+{
+	char		x = '.';
+	char		y = '*';
+	char		res;
+
+	std::cout << GRNCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
+	std::cout << MAGCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
+
+	res = min(x, y);
+	std::cout << "Who is the min ? " << std::endl << MAGCOLOR << res << ENDCOLOR << std::endl;
+
+	res = max(x, y);
+	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
+
+	std::cout << "-------------------------------" << std::endl;
+	swap(x, y);
+	std::cout << MAGCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
+	std::cout << GRNCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
+
+	res = min(x, y);
+	std::cout << "Who is the min ? " << std::endl << MAGCOLOR << res << ENDCOLOR << std::endl;
+
+	res = max(x, y);
+	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
+	return ;
+}
+
+void	floatTest( void )
+{
+	float		x = 42.42f;
+	float		y = 0.0f;
+	float		res;
+
+	std::cout << GRNCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
+	std::cout << MAGCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
+
+	res = min(x, y);
+	std::cout << "Who is the min ? " << std::endl << MAGCOLOR << res << ENDCOLOR << std::endl;
+
+	res = max(x, y);
+	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
+
+	std::cout << "-------------------------------" << std::endl;
+	swap(x, y);
+	std::cout << MAGCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
+	std::cout << GRNCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
+
+	res = min(x, y);
+	std::cout << "Who is the min ? " << std::endl << MAGCOLOR << res << ENDCOLOR << std::endl;
+
+	res = max(x, y);
+	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
+	return ;
+}
+
+void	boolTest( void )
+{
+	bool		x = true;
+	bool		y = false;
+	bool		res;
+
+	std::cout << GRNCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
+	std::cout << MAGCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
+
+	res = min(x, y);
+	std::cout << "Who is the min ? " << std::endl << MAGCOLOR << res << ENDCOLOR << std::endl;
+
+	res = max(x, y);
+	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
+
+	std::cout << "-------------------------------" << std::endl;
+	swap(x, y);
+	std::cout << MAGCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
+	std::cout << GRNCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
+
+	res = min(x, y);
+	std::cout << "Who is the min ? " << std::endl << MAGCOLOR << res << ENDCOLOR << std::endl;
+
+	res = max(x, y);
+	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
+	return ;
+}
+
+void	doubleTest( void )
+{
+	double		x = 42.42;
+	double		y = 0.0;
+	double		res;
+
+	std::cout << GRNCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
+	std::cout << MAGCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
+
+	res = min(x, y);
+	std::cout << "Who is the min ? " << std::endl << MAGCOLOR << res << ENDCOLOR << std::endl;
+
+	res = max(x, y);
+	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
+
+	std::cout << "-------------------------------" << std::endl;
+	swap(x, y);
+	std::cout << MAGCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
+	std::cout << GRNCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
+
+	res = min(x, y);
+	std::cout << "Who is the min ? " << std::endl << MAGCOLOR << res << ENDCOLOR << std::endl;
+
+	res = max(x, y);
+	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
+	return ;
+}
+
 int main(void)
 {
-    
-
-    return 0;
+	intTest();
+	// charTest();
+	// floatTest();
+	// doubleTest();
+	// boolTest();
+	return 0;
 }
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,15 +6,45 @@
 /*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 11:08:14 by wiozsert          #+#    #+#             */
-/*   Updated: 2022/10/15 11:09:52 by wiozsert         ###   ########.fr       */
+/*   Updated: 2022/10/15 11:36:56 by wiozsert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
+#include <iostream>
+#define REDCOLOR "\033[1;31m"
+#define GRNCOLOR "\033[1;32m"
+#define YELCOLOR "\033[1;33m"
+#define BLUECOLOR "\033[1;34m"
+#define MAGCOLOR "\033[1;35m"
+#define CYANCOLOR "\033[1;36m"
+#define ENDCOLOR "\033[0m"
+
 template<typename T>
+void	swap(T &x, T &y)
+{
+	T tmp = x;
+	
+	x = y;
+	y = tmp;
+}
 
+template<typename T>
+T const&	min(T const &x, T const &y)
+{
+	if (y <= x)
+		return y;
+	return x;
+}
 
+template<typename T>
+T const&	max(T const &x, T const &y)
+{
+	if (y >= x)
+		return y;
+	return x;
+}
 
 #endif
