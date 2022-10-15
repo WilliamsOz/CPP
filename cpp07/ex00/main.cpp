5e4c88c9abca1f2412bcd75a94084ce53dc19032
Williams Ozserttas <willyiony@zero.local>
2022-10-15 11:59:12 +0200

    End of ex00

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 11:09:59 by wiozsert          #+#    #+#             */
-/*   Updated: 2022/10/15 11:49:57 by wiozsert         ###   ########.fr       */
+/*   Updated: 2022/10/15 11:59:01 by wiozsert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	intTest( void )
 	res = max(x, y);
 	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
 
-	std::cout << "-------------------------------" << std::endl;
+	std::cout << "----------SWAP----------" << std::endl;
 	swap(x, y);
 	std::cout << MAGCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
 	std::cout << GRNCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
@@ -55,7 +55,7 @@ void	charTest( void )
 	res = max(x, y);
 	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
 
-	std::cout << "-------------------------------" << std::endl;
+	std::cout << "----------SWAP----------" << std::endl;
 	swap(x, y);
 	std::cout << MAGCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
 	std::cout << GRNCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
@@ -83,7 +83,7 @@ void	floatTest( void )
 	res = max(x, y);
 	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
 
-	std::cout << "-------------------------------" << std::endl;
+	std::cout << "----------SWAP----------" << std::endl;
 	swap(x, y);
 	std::cout << MAGCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
 	std::cout << GRNCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
@@ -111,7 +111,7 @@ void	boolTest( void )
 	res = max(x, y);
 	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
 
-	std::cout << "-------------------------------" << std::endl;
+	std::cout << "----------SWAP----------" << std::endl;
 	swap(x, y);
 	std::cout << MAGCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
 	std::cout << GRNCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
@@ -139,7 +139,7 @@ void	doubleTest( void )
 	res = max(x, y);
 	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << res << ENDCOLOR << std::endl;
 
-	std::cout << "-------------------------------" << std::endl;
+	std::cout << "----------SWAP----------" << std::endl;
 	swap(x, y);
 	std::cout << MAGCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
 	std::cout << GRNCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
@@ -152,12 +152,57 @@ void	doubleTest( void )
 	return ;
 }
 
+void	stringTest( void )
+{
+	std::string x = "String2";
+	std::string y = "String1";
+
+	std::cout << GRNCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
+	std::cout << MAGCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
+
+	std::cout << "Who is the min ? " << std::endl << MAGCOLOR << ::min( x, y ) << ENDCOLOR << std::endl;
+
+	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << ::max( x, y ) << ENDCOLOR << std::endl;
+
+	std::cout << "----------SWAP----------" << std::endl;
+	swap(x, y);
+	std::cout << MAGCOLOR << "I am x and my value is " << x << ENDCOLOR << std::endl;
+	std::cout << GRNCOLOR << "I am y and my value is " << y << ENDCOLOR << std::endl << std::endl;
+
+	std::cout << "Who is the min ? " << std::endl << MAGCOLOR << ::min( x, y ) << ENDCOLOR << std::endl;
+
+	std::cout << "Who is the max ? " << std::endl << GRNCOLOR << ::max( x, y ) << ENDCOLOR << std::endl;
+	return ;
+}
+
+void	subjectTest( void )
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return ;
+}
+
 int main(void)
 {
-	intTest();
+	subjectTest();
+	// intTest();
 	// charTest();
 	// floatTest();
 	// doubleTest();
 	// boolTest();
+	// stringTest();
 	return 0;
 }
