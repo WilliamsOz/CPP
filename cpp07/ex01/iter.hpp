5d0c1350050db0913fb38d178899912f581ba6cc
Williams Ozserttas <willyiony@zero.local>
2022-10-24 14:55:32 +0200

    End of ex01

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 13:43:04 by wiozsert          #+#    #+#             */
+/*   Updated: 2022/10/24 14:39:04 by wiozsert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+
+template<typename U>
+void	print(U *tab)
+{
+	std::cout << *tab << std::endl;
+}
+
+template<typename T>
+void	iter(T const &tab, unsigned long lenTab, void (*ptrFct)(const T))
+{
+	for (unsigned long i = 0 ; i < lenTab ; i++)
+		ptrFct(&tab[i]);
+}
+
+#endif
---
+++ cpp07/ex01/main.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 13:43:23 by wiozsert          #+#    #+#             */
+/*   Updated: 2022/10/24 14:55:18 by wiozsert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+
+void	integerTabTest( void )
+{
+	int	tab[13] = {
+		42, 21, 10, 5, 2, 1, 0, -1, -2, -5, -10, -21, -42
+	};
+
+	iter(tab, sizeof(tab)/sizeof(tab[0]), &print);
+	return ;
+}
+
+void	charTest( void )
+{
+	char	tab[6] = {
+		'c', 'c', 'c', 'n', '0', '5'
+	};
+
+	iter(tab, sizeof(tab)/sizeof(tab[0]), &print);
+	return ;
+}
+
+void	stringTabTest( void )
+{
+	std::string	tab[3] = {
+		"Bonjour", "Je m'apelle", "Willy"
+	};
+
+	iter(tab, sizeof(tab)/sizeof(tab[0]), &print);
+	return ;
+}
+
+int main( void )
+{
+	integerTabTest();
+	// stringTabTest();
+	// charTest();
+	return (0);
+}
