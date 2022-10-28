b943338692052acb2b12e6575adc0074ea3293f3
Williams OZSERTTAS <wiozsert@e1r7p12.clusters.42paris.fr>
2022-10-28 14:25:32 +0200

    Save

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 12:36:00 by wiozsert          #+#    #+#             */
-/*   Updated: 2022/10/27 14:34:28 by wiozsert         ###   ########.fr       */
+/*   Updated: 2022/10/28 13:14:26 by wiozsert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,15 @@
 #include <vector>
 #include <exception>
 #include "easyfind.tpp"
+#define REDCOLOR "\033[1;31m"
+#define GRNCOLOR "\033[1;32m"
+#define YELCOLOR "\033[1;33m"
+#define BLUECOLOR "\033[1;34m"
+#define MAGCOLOR "\033[1;35m"
+#define CYANCOLOR "\033[1;36m"
+#define ENDCOLOR "\033[0m"
+#define SC(x) std::cout<<x;
+#define EC std::cout<<ENDCOLOR;
 
 template<typename T>
 typename T::iterator	easyfind(T &src, int nb);
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,28 +6,114 @@
 /*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 12:35:40 by wiozsert          #+#    #+#             */
-/*   Updated: 2022/10/28 09:41:25 by wiozsert         ###   ########.fr       */
+/*   Updated: 2022/10/28 14:25:25 by wiozsert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
-int main( void )
+/*-------------------------------------------------------[Integer Test]-------------------------------------------------------*/
+void	integerFound( void )
 {
 	std::vector<int>	v(42, 100);
+	std::vector<int>::iterator	it;
 
-	std::vector<int>::const_iterator	it;
+	v.assign(42, 42);
+	try
+	{
+		it = easyfind(v, 42);
+		SC(GRNCOLOR)std::cout << "Occurence : [" << *it << "] is found" << std::endl;EC
+	}
+	catch (const std::exception &e)
+	{
+		SC(REDCOLOR)std::cout << e.what() << std::endl;EC
+	}
+	return ;
+}
 
-	for(it = v.begin() ; it < v.end() ; ++it)
-		std::cout << *it << std::endl;
-	// try
-	// {
-		// easyfind(v, 42);
-	// }
-	// catch(const std::exception& e)
-	// {
-		// std::cerr << e.what() << std::endl;
-	// }
-	
+void	integerNotFound( void )
+{
+	std::vector<int>	v(42, 100);
+	std::vector<int>::iterator	it;
+
+	try
+	{
+		it = easyfind(v, 42);
+		SC(GRNCOLOR)std::cout << "Occurence : [" << *it << "] is found" << std::endl;EC
+	}
+	catch (const std::exception &e)
+	{
+		SC(REDCOLOR)std::cout << e.what() << std::endl;EC
+	}
+	return ;
+}
+
+void	integerVectorTest( void )
+{
+	integerFound();
+	integerNotFound();
+	return ;
+}
+/*-------------------------------------------------------END OF INTEGER TEST-------------------------------------------------------*/
+
+
+
+/*-------------------------------------------------------[Char Test]-------------------------------------------------------*/
+void	charFound( void )
+{
+	std::vector<char>	v;
+	std::vector<char>::iterator	it;
+
+	for (int i = 0 ; i < 100 ; i++)
+		v.push_back('.');
+	v.at(42) = '*';
+	try
+	{
+		it = easyfind(v, 42);
+		SC(GRNCOLOR)std::cout << "Occurence : [" << *it << "] is found" << std::endl;EC
+	}
+	catch (const std::exception &e)
+	{
+		SC(REDCOLOR)std::cout << e.what() << std::endl;EC
+	}
+	return ;
+}
+
+void	charNotFound( void )
+{
+	std::vector<char>	v;
+	std::vector<char>::iterator	it;
+
+	for (int i = 0 ; i < 100 ; i++)
+		v.push_back('.');
+	try
+	{
+		it = easyfind(v, 42);
+		SC(GRNCOLOR)std::cout << "Occurence : [" << *it << "] is found" << std::endl;EC
+	}
+	catch (const std::exception &e)
+	{
+		SC(REDCOLOR)std::cout << e.what() << std::endl;EC
+	}
+	return ;
+}
+
+void	charVectorTest( void )
+{
+	charFound();
+	charNotFound();
+	return ;
+}
+/*-------------------------------------------------------END OF CHAR TEST-------------------------------------------------------*/
+
+
+
+
+
+int main( void )
+{
+	// integerVectorTest();
+	// charVectorTest();
+	// doubleVectorTest();
 	return 0;
 }
